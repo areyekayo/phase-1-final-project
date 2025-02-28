1918f335767e5e7a91468f9dbfd48ecd2053a44d
Riko <riko.fluchel@gmail.com>
2025-02-28 13:57:47 -0500

    add character search function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const characters = []
+const container = document.getElementById("character-container");
 
 document.addEventListener("DOMContentLoaded", () => {
     //fetch characters from API
@@ -7,32 +8,90 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             data["results"].forEach(character => addCharacter(character))
     })
+
+    const searchForm = document.getElementById("search-form")
+    searchForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const nameToSearch = document.getElementById("name-to-search").value;
+        searchName(nameToSearch);
+    })
 })
 
+function searchName(name){
+    /*
+    takes the input of character name search, finds matches in the characters array, and displays matched character cards
+    */
+    const nameCase = name.toLowerCase();
 
-function addCharacter(character){
+    //filter the characters array for matches on the character full name, first name, or last name
+    const matches = characters.filter(character => {
+        //split character name into different variables to match potential inputs
+        let [firstName, lastName] = character["Name"].toLowerCase().split(" ")
+        let fullName = character["Name"].toLowerCase()
+        
+        if (firstName === nameCase || lastName === nameCase || fullName === nameCase){
+            return character
+        }
+    })
+    // get the IDs of matched characters
+    const ids = [];
+    for (let match of matches){
+        let id = match["id"].toString();
+        ids.push(id)
+    }
+
+    // get an array of character cards
+    let cards = Array.from(document.getElementsByClassName("character-card"))
+    cards.forEach(card => {
+        //display cards if the card ID is in the matched IDs
+        const shouldDisplay = ids.includes(card.id)
+        if (shouldDisplay){
+            card.style.display = "block";
+        }
+        //if not, do not display the card
+        else {
+            card.style.display = "none";
+        }
+
+    })
+
+}
+
+function addCharacter(char){
+    /*
+    improvements: set a keys array for the character data I want to display.
+    instead of p elements, use UL and loop through keys to set li items for each attribute
+    */
+    const character = {
+        "id": char.id,
+        "Name": char.name,
+        "Status": char.status,
+        "Gender": char.gender,
+        "Species": char.species,
+        "Type": char.type,
+        "image": char.image
+    }
+
     characters.push(character)
     //create elements for character card
+    const keys = ["Name", "Status", "Gender", "Species", "Type"]
+    const ul = document.createElement("ul")
+    for (let key of keys){
+        const li = document.createElement("li");
+        li.textContent = `${key}: ${character[key]}`
+        li.className = key;
+        ul.append(li)
+    }
+
     const card = document.createElement("div");
-    const name = document.createElement("h3");
-    const status = document.createElement("p");
-    const species = document.createElement("p");
-    const type = document.createElement("p");
     const image = document.createElement("img");
-    const gender = document.createElement("p");
+    card.className = "character-card"
 
-    //set element content with character data
     card.id = character["id"]
-    name.textContent = `Name: ${character["name"]}`;
-    status.textContent = `Status: ${character["status"]}`;
+
     image.src = character["image"];
-    species.textContent = `Species: ${character["species"]}`;
-    gender.textContent = `Gender: ${character["gender"]}`;
-    type.textContent = `Type: ${character["type"]}`;
 
-    card.append(image, name, status, gender, species, type)
-
-    let container = document.getElementById("character-container");
+    card.append(image, ul)
     container.append(card)
 
 }
